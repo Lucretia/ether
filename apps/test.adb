cc692b8dbafaa032a8edf053484291818f6c191b
Luke A. Guest <laguest@archeia.com>
2012-07-07 16:21:22 +0100

    Some HTML corrections. Added the Ether.Initialise call. Also added a handler for when the directories do not exist.

--- apps/test.adb
+++ apps/test.adb
@@ -9,6 +9,7 @@ with Ether.Requests;
 with Ether.Responses;
 with AWS.Messages;
 with AWS.MIME;
+with Ada.Directories;
 
 use type Ether.Requests.Form_Data_Method;
 
@@ -24,7 +25,7 @@ procedure Test is
    Channel       : GNAT.Sockets.Stream_Access;
    Request       : Ether.Requests.Request;
 
-   Get_Login    : String :=
+   Get_Login     : String :=
      "<!DOCTYPE HTML PUBLIC ""-//W3C//DTD HTML 4.01//EN""" & CRLF &
      """http://www.w3.org/TR/html4/strict.Dtd"">" & CRLF &
      "<html>" & CRLF &
@@ -33,7 +34,7 @@ procedure Test is
      "  </head>" & CRLF &
      "  <body>" & CRLF &
      "    <h1>User Login</h1>" & CRLF &
-     "    <form method=""get"" accept-charset=""UTF-8"" enctype=""multipart/form-data"" action=""/get"">" & CRLF &
+     "    <form method=""get"" accept-charset=""UTF-8"" enctype=""application/x-www-form-urlencoded"" action=""/get"">" & CRLF &
      "      <p><label>Username : <input name=""username""></label></p>" & CRLF &
      "      <p><label>Password : <input name=""password"" type=""password""></label></p>" & CRLF &
      "      <p><label>Subscribe: <input name=""Subscribe"" type=""checkbox"" value=""Yes"" checked></label></p>" & CRLF &
@@ -52,25 +53,36 @@ procedure Test is
      "    </form>" & CRLF &
      "  </body>" & CRLF &
      "</html>" & CRLF;
-   
+
    Put_User_Login    : String :=
      "<!DOCTYPE HTML PUBLIC ""-//W3C//DTD HTML 4.01//EN""" & CRLF &
      """http://www.w3.org/TR/html4/strict.Dtd"">" & CRLF &
      "<html>" & CRLF &
      "  <head>" & CRLF &
-     "    <title>User Login</title>" & CRLF &
+     "    <title>User Login - POST</title>" & CRLF &
      "  </head>" & CRLF &
      "  <body>" & CRLF &
      "    <h1>User Login</h1>" & CRLF &
-     "    <form method=""post"" accept-charset=""UTF-32"" enctype=""multipart/form-data"" action=""/user"">" & CRLF &
-     "      <p><label>Username: <input name=""username""></label></p>" & CRLF &
-     "      <p><label>Password: <input name=""password""></label></p>" & CRLF &
+     "    <form method=""post"" accept-charset=""UTF-8"" enctype=""multipart/form-data"" action=""/user"">" & CRLF &
+     "      <p><label>Username: <input name=""username㋕""></label></p>" & CRLF &
+     "      <p><label>Password: <input name=""password"" type=""password""></label></p>" & CRLF &
      "      <p><label>Filename: <input type=""File"" name=""filename"" multiple=""true""></label></p>" & CRLF &
-     "      <p><button type=""submit"">Send</button></p>" & CRLF &
+     "      <p>" & CRLF &
+     "        <label for=""banana"">Banana</label>" & CRLF &
+     "        <input name=""fruit"" type=""radio"" value=""Banana"" id=""banana"" checked>" & CRLF &
+     "        <label for=""orange"">Orange</label>" & CRLF &
+     "        <input name=""fruit"" type=""radio"" value=""Orange"" id=""orange"">" & CRLF &
+     "        <label for=""mango"">Mango</label>" & CRLF &
+     "        <input name=""fruit"" type=""radio"" value=""Mango"" id=""mango"">" & CRLF &
+     "      </p>" & CRLF &
+     "      <p>" & CRLF &
+     "        <button type=""submit"">Send</button>" & CRLF &
+     "        <button type=""reset"">Reset</button>" & CRLF &
+     "    </p>" & CRLF &
      "    </form>" & CRLF &
      "  </body>" & CRLF &
      "</html>" & CRLF;
-   
+
    Put_Simple_Login  : String :=
      "<!DOCTYPE HTML PUBLIC ""-//W3C//DTD HTML 4.01//EN""" & CRLF &
      """http://www.w3.org/TR/html4/strict.Dtd"">" & CRLF &
@@ -82,12 +94,14 @@ procedure Test is
      "    <h1>User Login</h1>" & CRLF &
      "    <form method=""post"" accept-charset=""UTF-8"" enctype=""application/x-www-form-urlencoded"" action=""/simple"">" & CRLF &
      "      <p><label>Username: <input name=""username""></label></p>" & CRLF &
-     "      <p><label>Password: <input name=""password""></label></p>" & CRLF &
+     "      <p><label>Password: <input name=""password"" type=""password""></label></p>" & CRLF &
      "      <p><button type=""submit"">Send</button></p>" & CRLF &
      "    </form>" & CRLF &
      "  </body>" & CRLF &
      "</html>" & CRLF;
 begin
+   Ether.Initialise (Bytes => 3000, Upload_Dir => "./", Temp_Dir => "./t");
+
    -- for loop start
    Address.Addr := Addresses(Get_Host_By_Name(Host_Name), 1);
    Address.Port := 4000;
@@ -119,7 +133,7 @@ begin
       --  Put_Line("HTTP_Accept_Language : " & Ether.Requests.Value(Request, Ether.Requests.HTTP_Accept_Language));
       --  Put_Line("HTTP_Accept_Encoding : " & Ether.Requests.Value(Request, Ether.Requests.HTTP_Accept_Encoding));
       --  Put_Line("HTTP_Accept_Charset  : " & Ether.Requests.Value(Request, Ether.Requests.HTTP_Accept_Charset));
-      Put_Line("Content              : " & To_String(Ether.Requests.Content(Request)));
+      --  Put_Line("Content : " & To_String(Ether.Requests.Content(Request)));
 
       --        Ether.Responses.Create
 --          (Output  => Channel,
@@ -127,7 +141,7 @@ begin
 --           Mime    => Ether.Responses.Text_Plain,
 --           Content => "Ada Ether server running!" & CRLF &
 --                      "Requested URL: " & Ether.Requests.Value(Request, Ether.Requests.HTTP_Host) & Ether.Requests.Value(Request, Ether.Requests.Request_URI));
-      
+
       if Ether.Requests.Value(Request, Ether.Requests.Server_Name) = "newhome.co.uk" or
 	Ether.Requests.Value(Request, Ether.Requests.Server_Name) = "newhome.com" or
 	Ether.Requests.Value(Request, Ether.Requests.Server_Name) = "newhome.org" then
@@ -135,7 +149,7 @@ begin
       else
 	 Put_Line("---- OTHER");
       end if;
-	 
+
       declare
          --  URI          : String := Ether.Requests.Value
          --    (Request,
@@ -170,7 +184,7 @@ begin
             if Ether.Requests.Content_Length (Request) /= 0 then
 	       if Content_Type(Content_Type'First ..  AWS.MIME.Multipart_Form_Data'Last) =
 		 AWS.MIME.Multipart_Form_Data then
-	       
+
 		  Ether.Responses.Send
 		    (Output    => Channel,
 		     Status    => AWS.Messages.S200,
@@ -239,6 +253,11 @@ begin
 
    Close_Socket(Server_Socket);
 exception
+   when Ada.Directories.Name_Error =>
+      --  No sockets to close here so will raise a different exception which is confusing,
+      --  so we catch the name_error here and re-raise it so we get the correct error.
+      raise;
+
    when others =>
       Close_Socket(Socket);
       Close_Socket(Server_Socket);
