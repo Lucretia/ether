9d79be5ce56d188c284788855a6f06d0e05079f9
Luke A. Guest <laguest@archeia.com>
2012-07-07 16:18:14 +0100

    Added a temporary directory. Added some error checking on the directories passed otherwise it will not initialise the library.

--- src/ether.adb
+++ src/ether.adb
@@ -3,26 +3,51 @@
 --  Description     : Body root of the Ether SCGI library.
 --  Author          : Luke A. Guest
 --  Created On      : Thu May  3 15:32:35 2012
+with Ada.Directories;
+
 package body Ether is
-   procedure Initialise (Bytes : in Positive; Upload_Dir : in String) is
+   procedure Initialise
+     (Bytes      : in Positive;
+      Upload_Dir : in String;
+      Temp_Dir   : in String) is
+
+      package Dirs renames Ada.Directories;
    begin
       Max_Request_Bytes := Bytes;
-      Dir               := US.To_Unbounded_String (Upload_Dir);
-      Initialised       := True;
+
+      if Dirs.Exists (Upload_Dir) then
+         Uploads := US.To_Unbounded_String (Upload_Dir);
+
+         if Dirs.Exists (Temp_Dir) then
+            Temps       := US.To_Unbounded_String (Temp_Dir);
+            Initialised := True;
+         else
+            raise Dirs.Name_Error with
+              "[Ether] Temporary directory """ & Temp_Dir & """ does not exist!";
+         end if;
+      else
+         raise Dirs.Name_Error with
+           "[Ether] Upload directory """ & Upload_Dir & """ does not exist!";
+      end if;
    end Initialise;
-   
+
    function Is_Initialised return Boolean is
    begin
       return Initialised;
    end Is_Initialised;
-   
+
    function Max_Request_Size return Positive is
    begin
       return Max_Request_Bytes;
    end Max_Request_Size;
-   
+
    function Upload_Dir return String is
    begin
-      return US.To_String (Dir);
+      return US.To_String (Uploads);
    end Upload_Dir;
+
+   function Temp_Dir return String is
+   begin
+      return US.To_String (Temps);
+   end Temp_Dir;
 end Ether;
--- src/ether.ads
+++ src/ether.ads
@@ -8,20 +8,27 @@ with Ada.Strings.Unbounded;
 package Ether is
    --  Initialise the library with the max umber of bytes a request can have as content-type
    --  and the library's upload directory, i.e. where to save files to.
-   procedure Initialise (Bytes : in Positive; Upload_Dir : in String);
-   
+   procedure Initialise
+     (Bytes      : in Positive;
+      Upload_Dir : in String;
+      Temp_Dir   : in String);
+
    --  Has the library been initialisied?
    function Is_Initialised return Boolean;
-   
+
    --  Return the max number of bytes a request can have as it's content-type.
    function Max_Request_Size return Positive;
-   
+
    --  Get the directory to where the uploaded files are saved to.
    function Upload_Dir return String;
+
+   --  Get the directory where temporary files go.
+   function Temp_Dir return String;
 private
    package US renames Ada.Strings.Unbounded;
-   
+
    Initialised       : Boolean             := False;
    Max_Request_Bytes : Positive            := 2000;
-   Dir               : US.Unbounded_String := US.Null_Unbounded_String;
+   Uploads           : US.Unbounded_String := US.Null_Unbounded_String;
+   Temps             : US.Unbounded_String := US.Null_Unbounded_String;
 end Ether;
